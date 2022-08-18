9f1ecd04442f9c55626e1aac34fa0ef5d0185da7
dzulfikarmagang123 <111365072+dzulfikar05@users.noreply.github.com>
2022-08-18 14:06:55 +0700

    templating

---
+++ application/controllers/Main.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/sidebar');
+		$this->load->view('template/navbar');
+		$this->load->view('template/content');
+		$this->load->view('template/footer');
+	}
+}
---
+++ application/views/template/footer.php
@@ -0,0 +1,21 @@
+                <footer>
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2021 &copy; Ber-Empat</p>
+                        </div>
+                        <div class="float-end">
+                            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                                by <a href="https://ahmadsaugi.com">Saugi</a></p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+    <script src="<?= base_url()?>/template/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="<?= base_url()?>/template/js/bootstrap.bundle.min.js"></script>
+
+    <script src="<?= base_url()?>/template/js/main.js"></script>
+</body>
+
+</html>
